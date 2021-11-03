e378c8f1a56973c7708056a19e1534f63b4310ab
Radutiu Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2021-11-03 19:59:21 +0200

    small bug fix

--- src/components/chatheads/ChatHeads.jsx
+++ src/components/chatheads/ChatHeads.jsx
@@ -14,18 +14,20 @@ export default function ChatHeads({ items, setReceiver }) {
         </div>
       </div>
       <input className="chat-heads-search" placeholder="Search in Messenger" />
-      {items.map((obj, i) => (
-        <div
-          key={i}
-          className="chat-head-item"
-          onClick={() => setReceiver(obj)}
-        >
-          <div className="user-profile-pic-container">
-            <p className="user-profile-pic-text">{obj.email[0]}</p>
+      <div className="chat-heads-container">
+        {items.map((obj, i) => (
+          <div
+            key={i}
+            className="chat-head-item"
+            onClick={() => setReceiver(obj)}
+          >
+            <div className="user-profile-pic-container">
+              <p className="user-profile-pic-text">{obj.email[0]}</p>
+            </div>
+            <p>{obj.email}</p>
           </div>
-          <p>{obj.email}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
